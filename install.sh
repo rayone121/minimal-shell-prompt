7a3f3d96f9723ea3de678116c9a2e1f669282754
rayone121 <44477929+rayone121@users.noreply.github.com>
2021-11-12 20:05:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeede
 elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
 elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+elif [ -x "$(command -v pacman)" ];  then sudo pacman -S $packagesNeeded
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
 }
 
